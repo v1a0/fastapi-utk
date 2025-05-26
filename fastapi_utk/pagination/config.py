7c21374001a5b79a24eb9183b5a8b5952979edc8
v1a0 <contact@v1a0.dev>
2025-05-26 14:38:48 +0700

    fix url_page_size_param

--- fastapi_utk/pagination/config.py
+++ fastapi_utk/pagination/config.py
@@ -6,4 +6,4 @@ class PaginationConfig(BaseModel):
     default_page_size: int = 10
     max_page_size: int | None = 20
     url_page_param: str = "page"
-    url_page_size_param: str = "page_size"
+    url_page_size_param: str = "pageSize"
